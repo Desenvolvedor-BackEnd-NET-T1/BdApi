2cf77be1b87526655fccb41137a53cd26781d7cf
Vitor Lassen <vitor@lassen.com.br>
2026-09-01 21:43:11 -0300

    use required

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,12 +1,16 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace DbApi.Models
 {
     public class Cliente
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
         public string Nome { get; set; }
         public string Telefone { get; set; }
-        public string Email { get; set; }
+        [Required]
+        public  string Email { get; set; }
         public string Endereco { get; set; }
     }
 }
