92782df062d227393dd3a9187e0d3b2c5dcc0467
Vitor Lassen <vitor@lassen.com.br>
2026-09-01 21:18:58 -0300

    vadd swagger

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI( op =>
+    {
+        op.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 //app.UseHttpsRedirection();
