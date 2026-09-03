c85668c33c5ed3cb3a0700899f1fa8c2bca0a03b
Vitor Lassen <vitor@lassen.com.br>
2026-09-02 21:21:13 -0300

    update Cliente model with data annotations and rename columns in migration

---
+++ Migrations/20260903001947_updateTypesComDataAnnotation.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTypesComDataAnnotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "Clientes",
+                newName: "telefoneCli");
+
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Clientes",
+                newName: "nomeCli");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Clientes",
+                newName: "codCli");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "telefoneCli",
+                table: "Clientes",
+                type: "varchar(11)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nomeCli",
+                table: "Clientes",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "telefoneCli",
+                table: "Clientes",
+                newName: "Telefone");
+
+            migrationBuilder.RenameColumn(
+                name: "nomeCli",
+                table: "Clientes",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "codCli",
+                table: "Clientes",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,17 +1,26 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DbApi.Models
 {
     public class Cliente
     {
+        [Key]
+        [Column("codCli")]
+        
         public string Id { get; set; } = Guid.NewGuid().ToString();
         [Required]
+        [Column("nomeCli", TypeName ="varchar(150)") ]
+        [MaxLength(150)]
         public string Nome { get; set; }
+        [Column("telefoneCli", TypeName ="varchar(11)") ]
         public string Telefone { get; set; }
         [Required]
         public  string Email { get; set; }
         public string Endereco { get; set; }
         public DateTime DataNascimento { get; set; }
+
+    
     }
 }
